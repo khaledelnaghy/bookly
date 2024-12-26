22c2f342ff931b44b8878711b878381ae262586e
khaledelnaghy <khaledelnaghy777@gmail.com>
2024-12-27 01:09:32 +0200

    adding sliding animation to splash view

--- lib/features/splash/presentation/widgets/splash_body_view.dart
+++ lib/features/splash/presentation/widgets/splash_body_view.dart
@@ -1,8 +1,47 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/splash/presentation/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashBodyView extends StatelessWidget {
+class SplashBodyView extends StatefulWidget {
   const SplashBodyView({super.key});
+
+  @override
+  State<SplashBodyView> createState() => _SplashBodyViewState();
+}
+
+// SingleTickerProviderStateMixin => handle rate change value to run animation
+class _SplashBodyViewState extends State<SplashBodyView>
+    with SingleTickerProviderStateMixin {
+// AnimationController animationController => take value from 0 to 1
+  late AnimationController animationController;
+  //Animation object => take any values and pass it to animationController
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 2,
+      ),
+    );
+    slidingAnimation = Tween<Offset>(begin: Offset(0, 1.5), end: Offset.zero)
+        .animate(animationController);
+    // forward => run animation
+    animationController.forward();
+    
+      }
+
+
+       @override
+
+//  dispose => avoid leak memory
+  void dispose() {
+    
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +50,13 @@ class SplashBodyView extends StatelessWidget {
       // stretch => it will take all available space width
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo ),
-        const SizedBox(height: 5,),
-       const Text("Read Free Books" , textAlign: TextAlign.center,)
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 5,
+        ),
+
+        // AnimatedBuilder => handle part of update UI
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
