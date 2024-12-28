a0d8f75f93cd3b4fb9e4aacb50afc0cfcd47b296
khaledelnaghy <khaledelnaghy777@gmail.com>
2024-12-28 21:24:32 +0200

    add title & add rating to book details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book%20_image.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +18,32 @@ class BookDetailsViewBody extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * .16),
             child: CustomBookImage(),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            "The Jungle Book",
+            style: Styles.textStyle30,
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              "Rudyard Kipling",
+              style: Styles.textStyle18.copyWith(
+                // color: Color(0xff707070),
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500),
+              ),
+          ),
+             SizedBox(
+            height: 18,
+          ),
+          BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
 
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         Icon(
           FontAwesomeIcons.solidStar,
@@ -21,12 +24,18 @@ class BookRating extends StatelessWidget {
           "4.8",
           style: Styles.textStyle16,
         ),
-         SizedBox(
+        SizedBox(
           width: 5,
         ),
-         Text(
-          "2390",
-          style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            "2390",
+            style: Styles.textStyle14.copyWith(
+
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ),
       ],
     );
