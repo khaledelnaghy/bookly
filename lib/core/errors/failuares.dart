920984243e66c00eaff0bc624d8121a9e95807d9
khaledelnaghy <khaledelnaghy777@gmail.com>
2024-12-30 16:46:23 +0200

    create home repo method & fauilre file

---
+++ lib/core/errors/failuares.dart
@@ -0,0 +1,7 @@
+abstract class Failuare 
+{
+
+
+}
+
+class ServerFailure extends Failuare{}
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/core/errors/failuares.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  Future<void> fetchFeaturedBooks();
-  Future<void> fetchNewestBooks();
+  Future<Either<Failuare, List<BookModel>>> fetchFeaturedBooks();
+Future<Either<Failuare, List<BookModel>>> fetchBestSellerBooks();
 }
